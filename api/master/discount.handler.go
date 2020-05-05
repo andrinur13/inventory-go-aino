f4489c871aff66943b86ae7f017006de917d6e97
rino <rino@ainosi.co.id>
2020-05-05 20:05:12 +0700

    added get discount multidestination

--- api/master/discount.handler.go
+++ api/master/discount.handler.go
@@ -21,6 +21,7 @@ func DiscountRouter(r *gin.RouterGroup, permission middleware.Permission, cacheM
 	discount := r.Group("/discount")
 	{
 		discount.GET("/agent", permission.Set("PERMISSION_MASTER_USER_VIEW", GetDiscountAgent))
+		discount.GET("/multidestination", permission.Set("PERMISSION_MASTER_USER_VIEW", GetDiscountDest))
 	}
 }
 
@@ -32,6 +33,26 @@ func GetDiscountAgent(c *gin.Context) {
 	userData := middleware.Decode(split[1])
 
 	data, code, msg, stat := repositories.GetDiscountMulti(userData, "AGENT")
+
+	out, _ := json.Marshal(data)
+
+	contentLenght := len(string(out))
+
+	c.Header("Content-Type", "application/json; charset=utf-8")
+	c.Header("Response-Length", strconv.Itoa(contentLenght+76))
+	c.Header("Transfer-Encoding", "identity")
+	c.JSON(http.StatusOK, builder.ApiResponse(stat, msg, code, data))
+	logger.Info(msg, code, stat, fmt.Sprintf("%v", data), "")
+}
+
+// GetDiscountDest : get discount multidestination
+func GetDiscountDest(c *gin.Context) {
+	tokenString := c.Request.Header.Get("Authorization")
+	split := strings.Split(tokenString, " ")
+
+	userData := middleware.Decode(split[1])
+
+	data, code, msg, stat := repositories.GetDiscountMulti(userData, "MULTIDESTINATION")
 
 	out, _ := json.Marshal(data)
 
