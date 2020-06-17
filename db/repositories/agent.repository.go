b835a23c542cba92cdf5d5e09abeeb01d3340de7
elga lesmana <elga.lesmana@gmail.com>
2020-06-17 11:05:04 +0700

    detail agent

--- db/repositories/agent.repository.go
+++ db/repositories/agent.repository.go
@@ -46,6 +46,43 @@ func GetAgent() (*[]entities.AgentModel, string, string, bool) {
 	return &dataAgent, "01", "Get data agent success", true
 }
 
+//GetAgent : select data agent
+func GetDetailAgent(token *entities.Users) (*[]entities.AgentModel, string, string, bool) {
+	var agents []entities.AgentModel
+
+	if err := db.DB[0].Select(`agent_id, agent_name, agent_address,
+								agent_extras ->> 'agent_address_detail' as agent_address_detail,
+								agent_extras ->> 'telp' as telp,
+								agent_extras ->> 'no_id' as no_id,
+								agent_extras ->> 'email' as email,
+								agent_extras ->> 'pic_name' as pic_name`).Where("agent_id = ? AND deleted_at is null", token.Typeid).Order("agent_name").Find(&agents).Error; gorm.IsRecordNotFoundError(err) {
+		return nil, "02", "No agent data found (" + err.Error() + ")", false
+	}
+
+	var dataAgent []entities.AgentModel
+
+	for _, agent := range agents {
+		extras := entities.AgentExtras{
+			NoID:       agent.No_id,
+			AddrDetail: agent.Agent_address_detail,
+			PicName:    agent.Pic_name,
+			Telp:       agent.Telp,
+			Email:      agent.Email,
+		}
+
+		tmpAgent := entities.AgentModel{
+			Agent_id:      agent.Agent_id,
+			Agent_address: agent.Agent_address,
+			Agent_name:    agent.Agent_name,
+			AgentExtras:   &extras,
+		}
+
+		dataAgent = append(dataAgent, tmpAgent)
+	}
+
+	return &dataAgent, "01", "Get data agent success", true
+}
+
 //InsertAgent : insert data user
 func InsertAgent(token *entities.Users, r *entities.AgentReq) (map[string]interface{}, string, string, bool) {
 	if r.Agent == "" {
