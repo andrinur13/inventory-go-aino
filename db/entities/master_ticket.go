45d123f21a31a61610ea3bb0da93bf7a7dd6a915
rino <rino@ainosi.co.id>
2020-06-22 22:03:53 +0700

    remove some response attr on get ticket list

--- db/entities/master_ticket.go
+++ db/entities/master_ticket.go
@@ -1,26 +1,26 @@
 package entities
 
 type MasterTicket struct {
-	Trf_id            int        `json:"trf_id"`
-	Trf_name          string     `json:"trf_name"`
-	Trf_code          string     `json:"trf_code"`
-	Trf_agent_id      int        `json:"trf_agent_id"`
-	Trf_trftype       string     `json:"trf_trftype"`
-	Trf_currency_code string     `json:"trf_currency_code"`
-	Trf_value         float32    `json:"trf_value"`
-	Trf_label         string     `json:"trf_label"`
-	Trf_start_date    string     `json:"trf_start_date"`
-	Trf_end_date      string     `json:"trf_end_date"`
-	Trf_priority      int        `json:"trf_priority"`
-	Trf_release       string     `json:"trf_release"`
-	Trf_qty           int        `json:"trf_qty"`
-	Day               string     `json:"-"`
-	Begin_time        string     `json:"-"`
-	End_time          string     `json:"-"`
-	Card_type         string     `json:"-"`
-	Expired_qr        int        `json:"-"`
-	Trf_condition     *Condition `json:"trf_condition"`
-	Ticket            []Ticket   `json:"ticket"`
+	Trf_id   int    `json:"trf_id"`
+	Trf_name string `json:"trf_name"`
+	// Trf_code          string     `json:"trf_code"`
+	// Trf_agent_id      int        `json:"trf_agent_id"`
+	// Trf_trftype       string     `json:"trf_trftype"`
+	Trf_currency_code string  `json:"trf_currency_code"`
+	Trf_value         float32 `json:"trf_value"`
+	Trf_label         string  `json:"trf_label"`
+	// Trf_start_date    string     `json:"trf_start_date"`
+	// Trf_end_date      string     `json:"trf_end_date"`
+	// Trf_priority      int        `json:"trf_priority"`
+	// Trf_release       string     `json:"trf_release"`
+	// Trf_qty           int        `json:"trf_qty"`
+	// Day               string     `json:"-"`
+	// Begin_time        string     `json:"-"`
+	// End_time          string     `json:"-"`
+	// Card_type         string     `json:"-"`
+	// Expired_qr        int        `json:"-"`
+	// Trf_condition     *Condition `json:"trf_condition"`
+	// Ticket            []Ticket   `json:"ticket"`
 }
 
 type Ticket struct {
