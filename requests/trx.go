f61c4a5774fc29a3c9022de0b9f5bde0d9250b8c
rino <rino@ainosi.co.id>
2020-06-25 13:43:23 +0700

    mapping trx request

--- requests/trx.go
+++ requests/trx.go
@@ -33,3 +33,39 @@ type ValidateMemberData struct {
 	NoRef        string `json:"no_ref"`
 	DateValidate string `json:"date_validate"`
 }
+
+type TrxRes struct {
+	SourceType int       `json:"source_type"`
+	StartDate  string    `json:"start_date"`
+	EndDate    string    `json:"end_date"`
+	DestQty    int       `json:"destination_qty"`
+	Trip       []TrxTrip `json:"trip"`
+	Customer   []TrxCust `json:"customer"`
+}
+
+type TrxTrip struct {
+	TripDate string    `json:"trip_date"`
+	TripDay  int       `json:"trip_day"`
+	Ticket   []TrxTick `json:"ticket"`
+}
+
+type TrxCust struct {
+	Nationality string `json:"cs_nationality"`
+	Region      string `json:"cs_region"`
+	IDType      string `json:"cs_id_type"`
+	IDNumber    string `json:"cs_id_number"`
+	Name        string `json:"cs_name"`
+	Type        string `json:"cs_type"`
+	Title       string `json:"cs_title"`
+	Email       string `json:"cs_email"`
+	Phone       string `json:"cs_phone"`
+	IsPic       bool   `json:"cs_is_pic"`
+}
+
+type TrxTick struct {
+	Mmid         string `json:"mmid"`
+	SiteDuration string `json:"site_duration"`
+	TrfID        int    `json:"trf_id"`
+	TrfName      string `json:"trf_name"`
+	TrfQty       int    `json:"trf_qty"`
+}
