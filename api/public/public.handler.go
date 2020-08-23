b07bb13b11a5e9b4c569d3c67b886770ee18d7c7
rino <rino@ainosi.co.id>
2020-08-23 20:39:43 +0700

    added reset password

--- api/public/public.handler.go
+++ api/public/public.handler.go
@@ -1,14 +1,15 @@
 package public
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"twc-ota-api/db/entities"
 	"twc-ota-api/db/repositories"
 	"twc-ota-api/logger"
 	"twc-ota-api/middleware"
+	"twc-ota-api/requests"
 	"twc-ota-api/utils/builder"
-	"twc-ota-api/db/entities"
-	"encoding/json"
 
 	"github.com/gin-gonic/gin"
 )
@@ -17,6 +18,8 @@ import (
 func PublicRouter(r *gin.RouterGroup, permission middleware.Permission) {
 	r.GET("/agent", agent)
 	r.POST("/create/agent", RegisterAgent)
+	r.POST("/password/reset", ResetPassword)
+	r.POST("/password/update", UpdatePassword)
 }
 
 func agent(c *gin.Context) {
@@ -27,12 +30,37 @@ func agent(c *gin.Context) {
 	logger.Info(msg, code, stat, fmt.Sprintf("%v", data), "")
 }
 
+// RegisterAgent : register new agent
 func RegisterAgent(c *gin.Context) {
 	var req entities.AgentReq
 	c.BindJSON(&req)
 	in, _ := json.Marshal(req)
 
 	data, code, msg, stat := repositories.InsertAgent(nil, &req)
+
+	c.JSON(http.StatusOK, builder.ApiResponse(stat, msg, code, data))
+	logger.Info(msg, code, stat, fmt.Sprintf("%v", data), string(in))
+}
+
+// ResetPassword : reset password
+func ResetPassword(c *gin.Context) {
+	var req requests.ResetPassword
+	c.BindJSON(&req)
+	in, _ := json.Marshal(req)
+
+	data, code, msg, stat := repositories.ResetPassword(&req)
+
+	c.JSON(http.StatusOK, builder.ApiResponse(stat, msg, code, data))
+	logger.Info(msg, code, stat, fmt.Sprintf("%v", data), string(in))
+}
+
+// UpdatePassword : for update resetted password
+func UpdatePassword(c *gin.Context) {
+	var req requests.UpdateResetPassword
+	c.BindJSON(&req)
+	in, _ := json.Marshal(req)
+
+	data, code, msg, stat := repositories.UpdateResetPassword(&req)
 
 	c.JSON(http.StatusOK, builder.ApiResponse(stat, msg, code, data))
 	logger.Info(msg, code, stat, fmt.Sprintf("%v", data), string(in))
--- requests/user.go
+++ requests/user.go
@@ -5,3 +5,14 @@ type UpdatePassword struct {
 	NewPwd  string `json:"new_password"`
 	ConfPwd string `json:"confirm_password"`
 }
+
+type ResetPassword struct {
+	Email string `json:"email"`
+}
+
+type UpdateResetPassword struct {
+	Email   string `json:"email"`
+	Token   string `json:"token"`
+	NewPwd  string `json:"password"`
+	ConfPwd string `json:"confirm_password"`
+}
