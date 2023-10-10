eb3c8921fcd61e971a7829349f63abb1bb6f122f
fahmi <fahmi@ainosi.co.id>
2023-10-10 14:55:22 +0700

    Update booking models

--- db/entities/booking.go
+++ db/entities/booking.go
@@ -1,7 +1,7 @@
 package entities
 
 type Booking struct {
-	Booking_id             uint `gorm:"primary_key"`
+	Booking_id             uint
 	Agent_id               int
 	Booking_number         string
 	Booking_date           string
@@ -10,7 +10,7 @@ type Booking struct {
 	Booking_amount         float32
 	Booking_emoney         int
 	Booking_total_payment  float32
-	Booking_uuid           string `gorm:"type:uuid;not null;default:uuid_generate_v4()"`
+	Booking_uuid           string `gorm:"primary_key;type:uuid;not null;default:uuid_generate_v4()"`
 	Booking_redeem_date    string `gorm:"null;default:null"`
 	Booking_invoice        string
 	Customer_note          string
@@ -24,14 +24,14 @@ func (Booking) TableName() string {
 }
 
 type Bookingdet struct {
-	Bookingdet_id           uint `gorm:"primary_key"`
+	Bookingdet_id           uint
 	Bookingdet_booking_id   int
 	Bookingdet_trf_id       int
 	Bookingdet_trftype      string
 	Bookingdet_amount       float32
 	Bookingdet_qty          int
 	Bookingdet_total        float32
-	Bookingdet_uuid         string `gorm:"type:uuid;not null;default:uuid_generate_v4()"`
+	Bookingdet_uuid         string `gorm:"primary_key;type:uuid;not null;default:uuid_generate_v4()"`
 	Bookingdet_booking_uuid string `gorm:"type:uuid;not null;default:uuid_generate_v4()"`
 }
 
@@ -40,11 +40,11 @@ func (Bookingdet) TableName() string {
 }
 
 type Bookinglist struct {
-	Bookinglist_id              uint `gorm:"primary_key"`
+	Bookinglist_id              uint
 	Bookinglist_bookingdet_id   int
 	Bookinglist_mtick_id        int
 	Bookinglist_mid             string
-	Bookinglist_uuid            string `gorm:"type:uuid;not null;default:uuid_generate_v4()"`
+	Bookinglist_uuid            string `gorm:"primary_key;type:uuid;not null;default:uuid_generate_v4()"`
 	Bookinglist_bookingdet_uuid string `gorm:"type:uuid;not null;default:uuid_generate_v4()"`
 }
 
