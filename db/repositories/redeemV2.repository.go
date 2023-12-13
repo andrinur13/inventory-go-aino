8ba793c7c80bc27099db058aa2162d106e2d6372
Bima Sakti <bima.sakti@ainosi.co.id>
2023-12-13 21:49:20 +0700

    change second - to # to tickdetqr

--- db/repositories/redeemV2.repository.go
+++ db/repositories/redeemV2.repository.go
@@ -293,6 +293,25 @@ func RedeemTicketV2(userData *entities.Users, req *requests.RedeemReqV2) (map[st
 					for _, item := range qrData {
 						newTickdetID := uuid.NewV4()
 
+						qr := item.QR
+						new_qr := ""
+
+						firstIndex := strings.Index(qr, "-")
+						if firstIndex != -1 {
+							// Find the index of the second "-" after the first one
+							secondIndex := strings.Index(qr[firstIndex+1:], "-")
+							if secondIndex != -1 {
+								// Extract the substring until the second "-" and replace the second "-" with "#"
+								new_qr = qr[:firstIndex+secondIndex+1] + "#" + qr[firstIndex+secondIndex+2:]
+
+								fmt.Println(new_qr)
+							} else {
+								fmt.Println("String does not have enough '-' characters.")
+							}
+						} else {
+							fmt.Println("String does not have enough '-' characters.")
+						}
+
 						// start creating new tickdet
 						newTickDet := entities.TickDetModel{
 							Tickdet_id:      newTickdetID,
@@ -302,7 +321,7 @@ func RedeemTicketV2(userData *entities.Users, req *requests.RedeemReqV2) (map[st
 							Tickdet_amount:  item.TrfAmount,
 							Tickdet_qty:     1,
 							Tickdet_total:   item.TrfAmount,
-							Tickdet_qr:      item.QR,
+							Tickdet_qr:      new_qr,
 							Ext:             `{"void": {"status": false}, "refund": {"status": false}, "cashback": {"status": false}, "nationality": "ID"}`,
 						}
 
